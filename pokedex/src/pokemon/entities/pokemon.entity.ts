5377d77b77184f2a4042e7cadb6ee1028cfb2031
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2024-04-10 23:54:46 -0500

    S7 - db schema and model

--- pokedex/src/pokemon/entities/pokemon.entity.ts
+++ pokedex/src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,20 @@
-export class Pokemon {}
+import { Document } from "mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class Pokemon extends Document {
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    name: String;
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    no: number; //numero de pokemon
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
