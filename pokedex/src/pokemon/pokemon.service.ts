1a9b5808c0abf78dfa815a7bd3a3b442940c7255
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2024-05-18 13:49:38 -0500

    pokemon - custom pipe

--- pokedex/src/pokemon/pokemon.service.ts
+++ pokedex/src/pokemon/pokemon.service.ts
@@ -70,8 +70,11 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    const pokemon = await this.findOne(id);
-    await pokemon.deleteOne();
+    //const result = await this.pokemonModel.findByIdAndDelete(id);
+    const {deletedCount} = await this.pokemonModel.deleteOne({_id: id});
+
+    if (deletedCount === 0) throw new BadRequestException(`Pokemon with id ${id} not found`);
+
   }
 
   private handleExceptions(error: any) {
